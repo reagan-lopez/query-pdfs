5c7cf42f5acd6401cf4ff3bc16c5672792b4f42b
Reagan Lopez <reaganlopez@gmail.com>
2024-01-22 14:49:16 -0800

    Display previous messages in current session
    Signed-off-by: Reagan Lopez <reaganlopez@gmail.com>

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ from langchain.prompts import PromptTemplate
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.vectorstores import FAISS
 from PyPDF2 import PdfReader
-import streamlit as sl
+import streamlit as st
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
 
-# Extract text from pdf
+# Extract text from pdf.
 def get_pdf_text(pdfs):
     pdf_text = ""
     for pdf in pdfs:
@@ -25,26 +25,26 @@ def get_pdf_text(pdfs):
     return pdf_text
 
 
-# Split text into chunks
+# Split text into chunks.
 def get_text_chunks(pdf_text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     text_chunks = text_splitter.split_text(pdf_text)
     return text_chunks
 
 
-# Embed the chunks and insert into vector store
+# Embed the chunks and insert into vector store.
 def set_vector_store(text_chunks):
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("vector_store")
 
 
-# Define chain using model and prompt template
+# Define chain using model and prompt template.
 def get_chain():
     model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
 
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    provided context just say, "Sorry, I'm unable to fetch that information from the documents.", don't provide the wrong answer\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
 
@@ -58,11 +58,12 @@ def get_chain():
     return chain
 
 
+# Generate response using the vector store and model.
 def get_response(user_query):
     try:
         vector_store = FAISS.load_local("vector_store", embeddings=embeddings)
     except Exception as e:
-        return "Vector store is empty. Please upload PDFs."
+        return "Please upload PDFs to be queried."
 
     else:
         if not vector_store:
@@ -76,24 +77,47 @@ def get_response(user_query):
         return response["output_text"]
 
 
+# Format the message to be stored in the streamlit session state.
+def get_message(role, content):
+    return {"role": role, "content": content}
+
+
 if __name__ == "__main__":
-    sl.set_page_config("Query PDFs")
-    sl.header("Query PDFs using GeminiPro")
+    st.set_page_config("Query PDFs")
+    st.header("Query PDFs using GeminiPro")
 
     # Upload PDF section
-    with sl.sidebar:
-        sl.title("Menu:")
-        pdfs = sl.file_uploader("Upload PDFs:", accept_multiple_files=True)
-        if sl.button("Upload"):
-            with sl.spinner("Processing.."):
+    with st.sidebar:
+        st.title("Menu:")
+        pdfs = st.file_uploader("Upload PDFs:", accept_multiple_files=True)
+        if st.button("Upload"):
+            with st.spinner("Processing.."):
                 pdf_text = get_pdf_text(pdfs)
                 text_chunks = get_text_chunks(pdf_text)
                 set_vector_store(text_chunks)
-                sl.success("Done!")
+                st.success("Done!")
 
-    # User query section
-    user_query = sl.text_input("Enter query")
-    if user_query:
-        sl.write(get_response(user_query))
-    else:
-        sl.write("Please enter valid query!")
+    # Initialize streamlit session state
+    if "messages" not in st.session_state.keys():
+        message = get_message("assistant", "Upload PDFs and query.")
+        st.session_state.messages = [message]
+
+    # User prompt
+    if user_prompt := st.chat_input():
+        message = get_message("user", user_prompt)
+        st.session_state.messages.append(message)
+
+    # Display all messages in current session
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    # Generate last message
+    last_message = st.session_state.messages[-1]
+    if last_message["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Processing.."):
+                response = get_response(user_prompt)
+                st.write(response)
+                message = get_message("assistant", response)
+                st.session_state.messages.append(message)
